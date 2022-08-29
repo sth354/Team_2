feab84b6e3afd268a8b643d5fe04c83ceeac8477
frg13 <92691773+frg13@users.noreply.github.com>
2022-08-29 16:30:41 +0000

    First commit - Freyr

--- src/main/java/is/hi/hbv501g/team_2/Team2Application.java
+++ src/main/java/is/hi/hbv501g/team_2/Team2Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Team2Application {
-
+    //This is Freyr's comment
     public static void main(String[] args) {
         SpringApplication.run(Team2Application.class, args);
     }
