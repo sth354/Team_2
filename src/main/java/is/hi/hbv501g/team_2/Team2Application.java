98c78367568215cb162890a4da6065a7ab468318
AxelThorAsp <axel.aspelund@gmail.com>
2022-08-29 19:15:46 +0000

    Axel's first commit

--- src/main/java/is/hi/hbv501g/team_2/Team2Application.java
+++ src/main/java/is/hi/hbv501g/team_2/Team2Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Team2Application {
     //This is Freyr's comment
+    //Axel's commit
     public static void main(String[] args) {
         SpringApplication.run(Team2Application.class, args);
     }
