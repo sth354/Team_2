e96b22c77ce07fd455b830559b625cdec41a6807
sth354 <sth354@hi.is>
2022-08-31 14:54:48 +0000

    application runs

--- src/main/java/is/hi/hbv501g/team_2/Team2Application.java
+++ src/main/java/is/hi/hbv501g/team_2/Team2Application.java
@@ -2,8 +2,9 @@ package is.hi.hbv501g.team_2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class Team2Application {
     //This is Freyr's comment
     //Axel's commit and another commit from testbranch
