68179e5fddaf5f869ece3a3446335437377c3590
sth354 <sth354@hi.is>
2022-09-25 22:29:26 +0000

    it works!!!!!! again

--- src/main/java/is/hi/hbv501g/team_2/Controllers/GameController.java
+++ src/main/java/is/hi/hbv501g/team_2/Controllers/GameController.java
@@ -1,10 +1,13 @@
 package is.hi.hbv501g.team_2.Controllers;
 
+import is.hi.hbv501g.team_2.Persistence.Entities.Director;
 import is.hi.hbv501g.team_2.Services.DirectorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class GameController {
@@ -17,6 +20,8 @@ public class GameController {
 
     @RequestMapping("/game")
     public String gamePage(Model model) {
+
+        List<Director> directors = directorService.findAll();
 
         // make game... xD
 
