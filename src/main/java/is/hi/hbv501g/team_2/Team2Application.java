6e17714ee821dec73e539dd38e46732eb751f2ee
sth354 <sth354@hi.is>
2022-09-25 21:22:34 +0000

    stuff

--- src/main/java/is/hi/hbv501g/team_2/Team2Application.java
+++ src/main/java/is/hi/hbv501g/team_2/Team2Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication//(exclude = {DataSourceAutoConfiguration.class })
 public class Team2Application {
     //This is Freyr's comment
     //Axel's commit and another commit from testbranch
@@ -12,5 +12,4 @@ public class Team2Application {
     public static void main(String[] args) {
         SpringApplication.run(Team2Application.class, args);
     }
-
 }
