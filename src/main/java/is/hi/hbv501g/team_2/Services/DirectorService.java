25163794afe55255d00e9ddbe4ce66c6e1799eda
Bobbi11 <bjarnihaukur11@gmail.com>
2022-09-29 09:08:23 +0000

    added to interface

--- src/main/java/is/hi/hbv501g/team_2/Services/DirectorService.java
+++ src/main/java/is/hi/hbv501g/team_2/Services/DirectorService.java
@@ -4,6 +4,10 @@ import is.hi.hbv501g.team_2.Persistence.Entities.Director;
 
 import java.util.List;
 
+// Update view 
+
 public interface DirectorService {
     List<Director> findAll();
+
+    Director getRandomDirector()
 }
