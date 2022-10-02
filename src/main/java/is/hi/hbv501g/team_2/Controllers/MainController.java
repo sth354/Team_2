b9d04022584347b96223cc6dde2a9768d1513e5c
AxelThorAsp <axel.aspelund@gmail.com>
2022-10-02 17:46:43 +0000

    lives

--- src/main/java/is/hi/hbv501g/team_2/Controllers/MainController.java
+++ src/main/java/is/hi/hbv501g/team_2/Controllers/MainController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
-
     @RequestMapping("/")
     public String mainMenu() {
         //Business logic
