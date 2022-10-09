1d40b4eebf39bb4a781140fcfd56e744addff9b4
sth354 <sth354@hi.is>
2022-10-09 11:40:28 +0000

    first commit

--- src/main/java/is/hi/hbv501g/team_2/Controllers/UserController.java
+++ src/main/java/is/hi/hbv501g/team_2/Controllers/UserController.java
@@ -3,6 +3,7 @@ package is.hi.hbv501g.team_2.Controllers;
 import is.hi.hbv501g.team_2.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
@@ -10,5 +11,15 @@ public class UserController {
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @RequestMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
     }
 }
