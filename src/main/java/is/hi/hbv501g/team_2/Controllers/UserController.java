989dc536d7fedf92b22d754b1d0600417d75a534
sth354 <sth354@hi.is>
2022-10-09 11:36:34 +0000

    added Score stuff

--- src/main/java/is/hi/hbv501g/team_2/Controllers/UserController.java
+++ src/main/java/is/hi/hbv501g/team_2/Controllers/UserController.java
@@ -1,14 +1,14 @@
 package is.hi.hbv501g.team_2.Controllers;
 
+import is.hi.hbv501g.team_2.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class UserController {
-
-
+    private UserService userService;
     @Autowired
-    public UserController() {
-
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 }
