010f782d24325def840ed0e64c6b9ac0a2e2346a
AxelThorAsp <axel.aspelund@gmail.com>
2022-10-15 01:58:55 +0000

    Oops gleymdi aÃd commita... Equals and hashcode for Movie

--- src/main/java/is/hi/hbv501g/team_2/Persistence/Entities/Movie.java
+++ src/main/java/is/hi/hbv501g/team_2/Persistence/Entities/Movie.java
@@ -51,6 +51,22 @@ public class Movie {
     @Override
     public String toString() { return title + " (" + year + ")"; }
 
+    @Override
+    public int hashCode(){
+        return (int) (getID() ^ (getID() >>> 32));
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (o == null) return false;
+        if (this.getClass() != o.getClass()) return false;
+        Movie that = (Movie) o;
+        return ID == that.ID
+                && (title.equals(that.title)
+                && year.equals(that.year));
+    }
+
     public boolean equals(Movie movie) {
         return movie.getTitle().equals(title);
     }
